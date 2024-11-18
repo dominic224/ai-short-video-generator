466d888c750482511b38acef7e13f51fcd409cc6
dominic224 <dev007.js@gmail.com>
2024-11-18 15:43:31 +0700

    Update UI

--- app/dashboard/create-new/page.jsx
+++ app/dashboard/create-new/page.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import SelectTopic from "./_components/SelectTopic";
+import SelectStyle from "./_components/SelectStyle";
+import SelectDuration from "./_components/SelectDuration";
+import { Button } from "@/components/ui/button";
 
 function CreateNew() {
-  return <div>CreateNew</div>;
+  const [formData, setFormData] = useState([]);
+  const onHandleInputChange = (fieldName, fieldValue) => {
+    setFormData((prev) => ({
+      ...prev,
+      [fieldName]: fieldValue,
+    }));
+  };
+
+  return (
+    <div className="md:px-20">
+      <h2 className="font-bold text-4xl text-primary text-center">
+        Create New
+      </h2>
+
+      <div className="mt-10 shadow-md p-10">
+        {/** Select Topic */}
+        <SelectTopic onUserSelect={onHandleInputChange} />
+        {/** Select Style */}
+        <SelectStyle onUserSelect={onHandleInputChange} />
+        {/** Duration */}
+        <SelectDuration onUserSelect={onHandleInputChange} />
+        {/** Create Button */}
+        <Button className="mt-10 w-full">Create Short Video</Button>
+      </div>
+    </div>
+  );
 }
 
 export default CreateNew;
